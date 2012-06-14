6a0bb73e1e283ceb66142fa9afd66c0766aee501
Oliver Leics <oliver.leics@gmail.com>
2012-06-14 13:12:15 +0200

    Changes. Lots of changes.

--- filecache.js
+++ filecache.js
@@ -4,21 +4,50 @@ module.exports = filecache
 var fs = require('fs')
   , path = require('path')
   , mime = require('mime')
+  , EventEmitter = require('events').EventEmitter
+  , crypto = require('crypto')
+  , lstat = process.platform === 'win32' ? 'stat' : 'lstat'
+  ;
 
-function filecache(dir, cb) {
+
+function hash(d) {
+  var hash = crypto.createHash('sha1')
+  hash.update(d)
+  return hash.digest('hex')
+}
+
+function filecache(dir, watchChanges, cb) {
+  if(typeof watchChanges === 'function') {
+    cb = watchChanges
+    watchChanges = false
+  }
+  cb = cb || function() {}
   dir = path.resolve(dir)
+  
   var queue = [dir]
     , cache = {}
+    , em = new EventEmitter()
+    ;
+  
+  function handleError(err) {
+    em.emit('error', err)
+    cb(err)
+  }
   
   function read() {
-    if(queue.length === 0) return cb(null, cache)
+    if(queue.length === 0) {
+      em.emit('ready', cache)
+      return cb(null, cache)
+    }
+    
     p = queue.shift()
+    
     fs.stat(p, function(err, s) {
-      if(err) return cb(err)
+      if(err) return handleError(err)
       
       if(s.isDirectory()) {
         fs.readdir(p, function(err, files) {
-          if(err) return cb(err)
+          if(err) return handleError(err)
           queue = queue.concat(files.map(function(f) {
             return path.resolve(p, f)
           }))
@@ -26,20 +55,33 @@ function filecache(dir, cb) {
         })
       } else {
         fs.readFile(p, function(err, d) {
-          if(err) return cb(err)
+          if(err) return handleError(err)
           var pp = p.slice(0)
             , k = pp.slice(dir.length).replace(/\\/g, '/')
+            ;
+          
           cache[k] = d
+          cache[k].hash = hash(d)
+          cache[k].mtime = s.mtime
           cache[k].mime_type = mime.lookup(pp)
           
-          if(filecache.watchChanges) {
+          if(watchChanges) {
             fs.watch(pp, function() {
-              fs.readFile(pp, function(err, d) {
-                if(err) return console.error(err.stack||err)
-                var was = cache[k]
-                cache[k] = d
-                cache[k].mime_type = was.mime_type
-                _notifyer(k, cache[k], was, cache, dir)
+              fs.stat(pp, function(err, s) {
+                if(err) return em.emit('error', err)
+                
+                fs.readFile(pp, function(err, d) {
+                  if(err) return em.emit('error', err)
+                  
+                  var was = cache[k]
+                  
+                  cache[k] = d
+                  cache[k].hash = hash(d)
+                  cache[k].mtime = s.mtime
+                  cache[k].mime_type = was.mime_type
+                  
+                  em.emit('change', k, cache[k], was, cache, dir)
+                })
               })
             })
           }
@@ -51,12 +93,6 @@ function filecache(dir, cb) {
   }
   
   read()
-}
-
-filecache.watchChanges = false
-filecache.notifyChange = notify
-
-var _notifyer = function() {}
-function notify(cb) {
-  _notifyer = cb || function() {}
+  
+  return em
 }
